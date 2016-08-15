51fef864949767b1c5128a7d137bd323d7a4896c
Michael Heap <m@michaelheap.com>
2016-08-15 22:13:36 +0100

    Refactor to use a single class to interact with mas

--- mas.py
+++ mas.py
@@ -6,36 +6,53 @@ import os
 def main():
   module = AnsibleModule(
       argument_spec = dict(
-        state     = dict(default='present', choices=['present', 'absent']),
-        name      = dict(required=True),
-        mas_path   = dict(default=None)
+        state     = dict(default='present', choices=['present']),
+        id      = dict(required=False),
+        name      = dict(required=False),
+        mas_path   = dict(default=None),
+        update_cache   = dict(default=False)
         )
       )
 
-  mas = Mas(module)
-
-  # Is `mas` installed?
   try:
-    mas.check_installed()
+    # Is `mas` installed?
+    mas = Mas(module)
+    translator = MasProgramTranslator()
+
+    # Are we working with a name or an ID?
+    if module.params['id'] and module.params['name']:
+        raise Exception("You can't specify both 'id' and 'name' in the same call to mas")
+
+    # Update named cache if required. Do this *before* name conversion.
+    # This is a no-op for now until we work out how to distribute a 
+    # name to ID lookup list
+    if module.params['update_cache']:
+      translator.update_cache()
+
+    # Normalise to use an ID everywhere
+    if module.params['name']:
+        module.params['id'] = translator.convert(module.params['name'])
+
   except IOError as e:
     module.fail_json(msg=e.message)
   except Exception as e:
     module.fail_json(msg=e.message)
 
-  # At this point we can run commands
-  list = MasCommandList(module)
+  args = {"changed": False}
 
+  # Installation
   if module.params['state'] == 'present':
-    if list.is_installed(module.params['name']): 
-      module.exit_json(changed=False, name=module.params['name'])
-    else:
-      translator = MasProgramTranslator()
-      MasCommandInstall(module, translator, module.params['name'])
+    if not mas.is_installed(module.params['id']): 
+      err = mas.install(module.params['id'], module.check_mode)
+      if err != None:
+          module.fail_json(msg=err)
+      else:
+        args['changed'] = True
 
-  #if module.check_mode:
-  #    module.exit_json(changed=True, check_mode=True)
-
-  module.exit_json(changed=True, something_else=12345)
+  args['id'] = module.params['id']
+  if module.params['name']:
+    args['name'] = module.params['name']
+  module.exit_json(**args)
 
 class Mas(object):
 
@@ -44,7 +61,21 @@ class Mas(object):
     self.params = module.params
     self.mas_path = None
 
-  def check_installed(self):
+    # Populate caches
+    self.find_mas()
+    self._installed = self.cache_installed()
+
+  def cache_installed(self):
+    rc, raw_list, err = self.run(["list"])
+    rows = raw_list.split("\n")
+    installed = {}
+    for r in rows:
+      r = r.split(" ", 1)
+      if len(r) == 2:
+        installed[r[1]] = r[0]
+    return installed
+
+  def find_mas(self):
     path = self.params['mas_path']
     if path is None:
       path = 'mas'
@@ -74,42 +105,15 @@ class Mas(object):
     cmd.insert(0, self.mas_path)
     return self.module.run_command(cmd, False)
 
-class MasCommandList(Mas):
-  def __init__(self, module):
-    super(MasCommandList, self).__init__(module)
-    self.check_installed()
-    rc, out, err = self.run(["list"])
-    self.raw_list = out
-    self._installed = None
+  def install(self, id, dry_run):
+    if not dry_run:
+      rc, out, err = self.run(["install", id])
+      if rc != 0:
+          return "Error installing '{}': {}".format(id, err.rstrip())
+    return None
 
-  def process(self):
-    if self._installed is None:
-      rows = self.raw_list.split("\n")
-      self._installed = {}
-      for r in rows:
-        r = r.split(" ", 1)
-        if len(r) == 2:
-          self._installed[r[1]] = r[0]
-
-    return self._installed
-
-  def raw(self):
-    return self.process()
-
-  def is_installed(self, name):
-      return name in self.process()
-
-class MasCommandInstall(Mas):
-  def __init__(self, module, translator, name):
-    super(MasCommandInstall, self).__init__(module)
-    self.translator = translator
-    self.check_installed()
-    self.install(name)
-
-  def install(self, name):
-    rc, out, err = self.run(["install", self.translator.convert(name)])
-    #print out
-    # 411246225 Caffeine
+  def is_installed(self, id):
+      return id in self._installed.values()
 
 class MasProgramTranslator(object):
   def __init__(self):
@@ -126,6 +130,8 @@ class MasProgramTranslator(object):
       return str(self.list[name])
     raise Exception("Unable to find the application '{}'".format(name))
 
+  def update_cache(self):
+      return True
 
 if __name__ == '__main__':
   main()
